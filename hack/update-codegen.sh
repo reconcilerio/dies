21d0a466d8fe9c2febf42ebadb558887f8b2dde2
Scott Andrews <scott@andrews.me>
2022-03-20 14:02:41 -0400

    Use go 1.18
    - introduce go.work file to reference nested modules
    - use `go generate` to call diegen
    - move ./diegen/testdata -> ./sandbox
    - cleanup ./hack/update-codegen.sh script
    
    Signed-off-by: Scott Andrews <scott@andrews.me>

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -4,15 +4,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}")/.. && pwd)
-
-(cd "${REPO_ROOT}/diegen" ; go build)
+cd $(dirname "${BASH_SOURCE[0]}")/..
 
 # genreate apis
-(cd "${REPO_ROOT}" ; ./diegen/diegen die:headerFile="hack/boilerplate.go.txt" paths="./apis/...")
-(cd "${REPO_ROOT}" ; go mod tidy)
-(cd "${REPO_ROOT}" ; go test -cover ./apis/...)
-
-# genreate diegen testdata
-(cd "${REPO_ROOT}/diegen" ; rm -f testdata/zz_generated.die*)
-(cd "${REPO_ROOT}/diegen" ; ./diegen die:headerFile="../hack/boilerplate.go.txt" paths="./testdata")
+go generate ./...
+go mod tidy
+go test ./...
